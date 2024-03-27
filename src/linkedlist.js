8e7220f071439826d91ccc2fd5ba3a5e424cbc32
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-03-27 11:24:40 +0100

    Add append function

--- src/linkedlist.js
+++ src/linkedlist.js
@@ -1,6 +1,6 @@
 // src/linkedlist.js
 
-import { Node } from "./node";
+import Node from "./node";
 
 
 
@@ -8,89 +8,32 @@ export default class LinkedList{
     constructor(){
         this.head = null;
     }
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Old code examples for inspiration
-
-/*
-
-class Todo {
-    constructor(description){
-        this.description = description;
-        this.completed = false;
-    }
-
-    toggleCompleted() {
-        this.completed = !this.completed;
-    }
-}
-
-class Project {
-    constructor(name, todos){
-        this.name = name;
-        this.todos = [];
-    }
-
-    addTodo(description){
-        const todo = new Todo(description);
-        this.todos.push(todo);
-    }
-
-    removeTodoByDescription(description){
-        const index = this.todos.findIndex(todo => todo.description === description);
-        if(index !== 1) {
-            this.todos.splice(index, 1);
-            console.log(description);
-            console.log(" deleted");
+    append(value){
+        if(this.head == null) {
+            const newNode = new Node(value, null);
+            this.head = newNode;
+            console.log(`Empty - this was added ${  value}`);
+        } else {
+            let currentNode = this.head;
+            while (currentNode.nextNode !== null){
+                currentNode = currentNode.nextNode;
+            }
+            currentNode.nextNode = new Node(value, null)
+            console.log(`this was added ${  value}`);
         }
-        
-    }
-
-    listTodos() {
-        console.log(`Todos for project: ${this.name}`);
-        this.todos.forEach((todo, index) => {
-            console.log(`${index + 1}: ${todo.description} - Completed: ${todo.completed}`);
-        });
-    }
-}
-
-export class TodoApp{
-    constructor(){
-        this.projects = [];
-    }
-
-    addProject(name) {
-        const project = new Project(name);
-        this.projects.push(project);
-    }
-
-    // After adding projects to myTodoApp
-    listProjects(){
-    this.projects.forEach(project => console.log(project.name))};
-
-    findProjectByName(name){
-        return this.projects.find(project => project.name === name);
     }
 
 
-    static Todo = Todo;
-
-    static Project = Project;
 }
 
 
 
 
-*/   
+
+
+
+
+
+
